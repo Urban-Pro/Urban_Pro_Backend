feac48da7fd9fad77ff46fb359ec6277edabe740
CandyPage <manuelagiraldotuberquia@gmail.com>
2023-03-10 04:24:46 -0500

    asd

--- routes/telegramRoutes.js
+++ routes/telegramRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 import { telegram } from "../controllers/telegramController.js";
 
 // Telegram API
-router.post("/", telegram ); // Manejar Entrada Telegram
+router.post("/file", telegram ); // Manejar Entrada Telegram
 
 export default router;
